e6a208806fca529326fa119410f15770dab39de5
Keval <keval.langalia@live.com>
2021-02-04 04:53:21 +0530

    First Run - successful
    - setup navigation & HomeActivity
    - added animations

--- app/src/main/java/com/example/pharmacymanagement/HomeActivity.kt
+++ app/src/main/java/com/example/pharmacymanagement/HomeActivity.kt
@@ -2,10 +2,30 @@ package com.example.pharmacymanagement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.example.pharmacymanagement.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var navController: NavController
+    // private lateinit var homeViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
+
+        navController = this.findNavController(R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        // return NavigationUI.navigateUp(navController, AppBarConfiguration(navController.graph))
+        return navController.navigateUp()
     }
 }
