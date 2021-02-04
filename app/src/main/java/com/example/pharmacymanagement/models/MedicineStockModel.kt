21776d81172f05676c8df0ea4bd3f3156df85d70
Keval <keval.langalia@live.com>
2021-02-04 12:36:12 +0530

    mostly UI & Floating hint edittext by textInputLayout

--- app/src/main/java/com/example/pharmacymanagement/models/MedicineStockModel.kt
+++ app/src/main/java/com/example/pharmacymanagement/models/MedicineStockModel.kt
@@ -5,20 +5,20 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 class MedicineStockModel(
-    medicine_name: String,
-    stock_quantity: Int,
-    description: String
+    val name: String,
+    val quantity: Int,
+    val desc: String
 ) : Parcelable {
 
-    private var _medicine_name: String = medicine_name
+    private var _medicine_name: String = name
     val medicine_name: String
         get() = _medicine_name
 
-    private var _stock_quantity: Int = stock_quantity
+    private var _stock_quantity: Int = quantity
     val stock_quantity: Int
         get() = _stock_quantity
 
-    private var _description: String = description
+    private var _description: String = desc
     val description: String
         get() = _description
 
